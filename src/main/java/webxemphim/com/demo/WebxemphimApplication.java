da9d45bceefe2844f468b5b8d443f212cb54346a
DESKTOP-JC8STTK\admin <toanhd290803@gmail.com>
2024-02-28 20:59:02 +0700

    upload file

---
+++ src/main/java/webxemphim/com/demo/WebxemphimApplication.java
@@ -0,0 +1,13 @@
+package webxemphim.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebxemphimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebxemphimApplication.class, args);
+    }
+
+}
