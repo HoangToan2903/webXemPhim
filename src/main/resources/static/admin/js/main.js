fd4e791e7e1ce10e8d478fc06dff3dc1d500c03d
DESKTOP-JC8STTK\admin <toanhd290803@gmail.com>
2024-04-24 13:35:28 +0700

    upload file

---
+++ src/main/resources/static/admin/js/main.js
@@ -0,0 +1,11 @@
+(function($) {
+
+	"use strict";
+
+	 $(document).ready(function() {
+        $('#multiple-checkboxes,#multiple-checkboxess,#multiple-checkboxesss ').multiselect({
+          includeSelectAllOption: true,
+        });
+    });
+	 
+})(jQuery);
